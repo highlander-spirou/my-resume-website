e926ac1e92de9d383fccdef66ebac0ac015f8b43
highlander-spirou <highlandetspirou2812@gmail.com>
2023-07-31 00:08:09 +0700

    working on modal

--- landing/src/components/Modal/Modal.jsx
+++ landing/src/components/Modal/Modal.jsx
@@ -1,6 +1,6 @@
 import { Dialog } from "@kobalte/core";
 import { createSignal, createEffect } from "solid-js";
-import { $isModalOpen, toggleModalState } from "@store/modalState"
+import { $isModalOpen, toggleModalState, $modalImg } from "@store/modalState"
 import { useStore } from '@nanostores/solid';
 import './Modal.css'
 
@@ -8,24 +8,22 @@ const Modal = () => {
     // const [open, setOpen] = createSignal(false);
 
     const isModalOpen = useStore($isModalOpen)
+    const modalImg = useStore($modalImg)
     const handleOpenChange = (e) => {
         toggleModalState(e ? 1 : 0)
     }
 
+    createEffect(() => {
+        console.log(modalImg())
+    })
+
     return (
         <>
             <Dialog.Root open={isModalOpen() === 1} onOpenChange={handleOpenChange}>
-                {/* <Dialog.Trigger class="dialog__trigger">Open</Dialog.Trigger> */}
                 <Dialog.Portal>
                     <Dialog.Overlay class="dialog__overlay" />
                     <div class="dialog__positioner">
                         <Dialog.Content class="dialog__content">
-                            <div class="dialog__header">
-                                <Dialog.Title class="dialog__title">About Kobalte</Dialog.Title>
-                                <Dialog.CloseButton class="dialog__close-button">
-                                    X
-                                </Dialog.CloseButton>
-                            </div>
                             <Dialog.Description class="dialog__description">
                                 Kobalte is a UI toolkit for building accessible web apps and design systems with
                                 SolidJS. It provides a set of low-level UI components and primitives which can be the
--- landing/src/store/modalState.js
+++ landing/src/store/modalState.js
@@ -1,10 +1,17 @@
 import { atom, action } from 'nanostores'
 
+
 const $isModalOpen = atom(0)
 
 const toggleModalState = action($isModalOpen, 'toggleModalState', (store, value) => {
     store.set(value)
 })
 
-export { $isModalOpen, toggleModalState }
+const $modalImg = atom("IELTS")
+
+const switchImg = action($modalImg, 'switchImg', (store, value) => {
+    store.set(value)
+})
+
+export { $isModalOpen, toggleModalState, $modalImg, switchImg }
 
